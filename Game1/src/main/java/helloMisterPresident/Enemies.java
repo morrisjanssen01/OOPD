ecd2c084e7361d4ed2ad67a3ef0283374b755fed
morrisjanssen01 <45886805+morrisjanssen01@users.noreply.github.com>
2019-04-01 11:54:58 +0200

    -

--- Game1/src/main/java/helloMisterPresident/Enemies.java
+++ Game1/src/main/java/helloMisterPresident/Enemies.java
@@ -1,10 +1,12 @@
 package helloMisterPresident;
 
-public class Enemies extends Entities {
+public abstract class Enemies extends Entities {
 
 	public Enemies(int x, int y, int grootte) {
 		super(x, y, grootte);
 		// TODO Auto-generated constructor stub
 	}
+	
+	public abstract void beweeg();
 
 }
