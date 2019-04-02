52c2ebedace4db44d66b0e0d24b04948793309ee
lars092000 <larsboxmeer@gmail.com>
2019-04-02 13:38:35 +0200

    poging tot afmaken enemies

--- Game1/src/main/java/helloMisterPresident/Enemies.java
+++ Game1/src/main/java/helloMisterPresident/Enemies.java
@@ -1,10 +1,23 @@
 package helloMisterPresident;
 
-public class Enemies extends Entities {
+import nl.han.ica.oopg.objects.AnimatedSpriteObject;
+import nl.han.ica.oopg.objects.Sprite;
 
-	public Enemies(int x, int y, int grootte) {
-		super(x, y, grootte);
+public class Enemies extends AnimatedSpriteObject {
+
+	public Enemies(int x, int y, int grootte, Sprite sprite) {
+		this.x = x;
+		this.y = y;
+		this.grootte = grootte;
+		super(new Sprite(HelloMisterPresident.MEDIA_URL.concat(this.sprite), 2))
+		
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- Game1/src/main/java/helloMisterPresident/Projectiel.java
+++ Game1/src/main/java/helloMisterPresident/Projectiel.java
@@ -1,11 +1,27 @@
 package helloMisterPresident;
 
 import java.util.List;
+import nl.han.ica.oopg.objects.Sprite;
 
 import nl.han.ica.oopg.collision.ICollidableWithGameObjects;
 import nl.han.ica.oopg.objects.GameObject;
 
 public class Projectiel implements ICollidableWithGameObjects {
+	private int x;
+	private int y;
+	private float velocity;
+	private int richting;
+	
+	Projectiel projectiel = new Projectiel(x,y, velocity, richting);
+	
+	
+	public Projectiel(int x, int y, float velocity, int richting) {
+		this.x = x;
+		this.y = y;
+		this.velocity = velocity;
+		this.richting = richting;
+	}
+
 
 	@Override
 	public void gameObjectCollisionOccurred(List<GameObject> collidedGameObjects) {
