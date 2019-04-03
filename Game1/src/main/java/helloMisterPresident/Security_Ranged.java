64d2cb39edcf7b859c3369c2f1738ef800ddb641
lars092000 <larsboxmeer@gmail.com>
2019-04-03 16:30:58 +0200

    projectielrichting

--- Game1/src/main/java/helloMisterPresident/Security_Ranged.java
+++ Game1/src/main/java/helloMisterPresident/Security_Ranged.java
@@ -1,5 +1,6 @@
 package helloMisterPresident;
 
+import nl.han.ica.oopg.alarm.Alarm;
 import nl.han.ica.oopg.alarm.IAlarmListener;
 import nl.han.ica.oopg.objects.Sprite;
 
@@ -9,14 +10,19 @@ public class Security_Ranged extends Enemies implements IAlarmListener{
 	private float speed = 5;
 	private int Punt1;
 	private int Punt2;
+	private Alarm alarm;
+	private final HelloMisterPresident world;
+	private int schietRichting;
+	private double schietDelay = 3;
 	
 	
-	public Security_Ranged(int x, int y, Sprite sprite) {
+	public Security_Ranged(int x, int y, Sprite sprite, HelloMisterPresident world) {
 		super(x, y, new Sprite(HelloMisterPresident.MEDIA_URL.concat("PNG/Characters/mon2_sprite_base1.png"))); //sprite nog aanpassen
 		this.richting = richting;
 		this.Punt1 = (int) this.x;
 		this.Punt2 = this.Punt1 + 100;
 		setCurrentFrameIndex(0);
+		this.world = world;
 	}
 
 	@Override
@@ -36,10 +42,30 @@ public class Security_Ranged extends Enemies implements IAlarmListener{
 		
 	}
 
+	public void getSchietRichting() {
+		if (this.x >= getX()) {
+		schietRichting = 1;
+		}
+		else {
+			schietRichting = 0;
+			}
+	}
+	public void schiet() {
+		getSchietRichting();
+		Projectiel projectiel = new Projectiel(this.x,this.y, 4f, schietRichting);
+		world.addGameObject(projectiel,(float)this.x, (float)this.getCenterY());
+	}
+	
+	public void startAlarm(){
+	alarm = new Alarm("Schieten", schietDelay);
+	alarm.addTarget(this);
+	alarm.start();
+	}
+	
 	@Override
 	public void triggerAlarm(String alarmName) {
-		// TODO Auto-generated method stub
-		
+		Projectiel p = new Projectiel(this.x,this.y, 4f, this.richting);
+		world.addGameObject(p,(float)this.x, (float)this.getCenterY());
 	}
-
+	
 }
