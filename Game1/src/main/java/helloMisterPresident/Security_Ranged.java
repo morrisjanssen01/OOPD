4cf5540c60427fe410ea3edc937c3b623aa3d0d6
lars092000 <larsboxmeer@gmail.com>
2019-04-03 09:35:02 +0200

    -

--- Game1/src/main/java/helloMisterPresident/Security_Ranged.java
+++ Game1/src/main/java/helloMisterPresident/Security_Ranged.java
@@ -1,5 +1,27 @@
 package helloMisterPresident;
 
-public class Security_Ranged {
+import nl.han.ica.oopg.objects.Sprite;
+
+public class Security_Ranged extends Enemies{
+
+	private int richting;
+	private float speed = 5;
+	private int Punt1;
+	private int Punt2;
+	
+	
+	public Security_Ranged(int x, int y, Sprite sprite) {
+		super(x, y, new Sprite(HelloMisterPresident.MEDIA_URL.concat("PNG/Characters/mon2_sprite_base1.png"))); //sprite nog aanpassen
+		this.richting = richting;
+		this.Punt1 = (int) this.x;
+		this.Punt2 = this.Punt1 + 100;
+		setCurrentFrameIndex(0);
+	}
+
+	@Override
+	public void beweeg() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
